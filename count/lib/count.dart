9f6f1003800186382406a75116e4bed5c5c0befd
gopheramit <amitagrawal10@outlook.com>
2020-11-14 11:17:01 +0530

    almost working

--- count/lib/count.dart
+++ count/lib/count.dart
@@ -83,7 +83,7 @@ class _CounterViewState extends State<CounterView1> {
 
   void _dicrement() {
     setState(() {
-      if (_currentCount > 0) {
+      if (_currentCount >= 0) {
         _currentCount--;
         couponsavail++;
       }
@@ -99,7 +99,7 @@ class _CounterViewState extends State<CounterView1> {
 
   void _dicrementDin() {
     setState(() {
-      if (_currentCountDinn > 0) {
+      if (_currentCountDinn >= 0) {
         _currentCountDinn--;
         couponsavail++;
       }
@@ -115,7 +115,7 @@ class _CounterViewState extends State<CounterView1> {
 
   void _dicrementBra() {
     setState(() {
-      if (_currentCount > 0) {
+      if (_currentCount >= 0) {
         _currentCountBrake--;
         couponsavail++;
       }
